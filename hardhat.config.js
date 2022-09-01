0d8c3f95cf866122e03c31b7079ff48f63849472
okolicodes7 <okolifavour50@gmail.com>
2022-09-02 00:03:37 +0100

    seventh commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
+// Whitelist Contract Address: 0xDEE1E423e6d84C3f9cAaA71fC7B0b59447aBfb77
+
+
